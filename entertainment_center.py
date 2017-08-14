79b5735334e720347a822d6d75ce7c1714e2bcda
edwinaquino <github@edwinaquino.com>
2017-08-14 15:50:39 -0700

    Fixed styling errors and corrected spelling, ready for resubmission 2

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,25 +7,25 @@ import fresh_tomatoes
 toy_story = media.Movie(
     "Toy Story",
     "A story of a boy and hist toys that come to life",
-    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
     "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie(
     "Avatar",
     "A marine on an alien planet",
-    "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
     "https://youtu.be/5PSNL1qE6VY?t=10s")
 
 departed = media.Movie(
     "The Departed",
     "An undercover cop and a mole in the police department",
-    "http://www.freemovieposters.net/posters/departed_the_2006_1253_poster.jpg",
+    "http://www.freemovieposters.net/posters/departed_the_2006_1253_poster.jpg",   # noqa
     "https://www.youtube.com/watch?v=auYbpnEwBBg")
 
 matrix = media.Movie(
   "The Matrix",
   "A group of free humans battle machine",
-  "http://si.wsj.net/public/resources/images/BN-BF325_0123gh_J_20140123153621.jpg",  # noqa
+  "http://www.overimage.com/temp/matrix2.jpg",
   "https://www.youtube.com/watch?v=vKQi3bBA1y8")
 
 rush_hour = media.Movie(
@@ -37,7 +37,7 @@ rush_hour = media.Movie(
 fellowship_of_the_ring = media.Movie(
     "The Fellowship of the Ring",
     " Tolkien's classic fantasy novel.",
-    ("https://www.overimage.com/temp/ring.jpg"),
+    ("http://www.overimage.com/temp/ring.jpg"),
     "https://www.youtube.com/watch?v=V75dMMIW2B4")
 
 movies = [
--- media.py
+++ media.py
@@ -14,6 +14,15 @@ class Movie():
             poster_image,
             trailer_youtube
     ):
+        """
+        This is the constructor method for the Movie class.
+        It is used to setup all the variables in the Movie class.
+        Reguired arguments are:
+        Movie Title
+        Movie Story Line/Description
+        URL to Movie Poster Image (example: http://example.com/image.jpg)
+        Youtube trailer URL. (example: https://www.youtube.com/watch?v=vKQi3bBA1y8)   # noqa
+        """
 
         self.title = movie_title
         self.storyline = movie_storyline
@@ -21,4 +30,7 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        Opens the youtube trailer by using the webbrowser module
+        """
         webbrowser.open(self.trailer_youtube_url)
